1eaac63cb9f035b24b1caacaa142cb6281f3603a
Spencer Rauch <rauchs@gmail.com>
2023-02-15 08:47:38 -0800

    algo

---
+++ Day3/algo/countpos.js
@@ -0,0 +1,10 @@
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+// your code here!
+console.log("there are " + countPositives + " positive values");
+/*
+Bonus Challenge:
+Level 1: Write this as a function that accepts arrays as parameters.
+Level 2: Include a return statement that gives back the counted positive numbers.
+Level 3: Instead of returning the count, return a new array of only the positive numbers.
+*/
