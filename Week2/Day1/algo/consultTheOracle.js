14fda8b8a4ed7386829eca5835268bc72568e4b7
Spencer Rauch <rauchs@gmail.com>
2023-02-20 08:07:22 -0800

    updated algo

--- Week2/Day1/algo/consultTheOracle.js
+++ Week2/Day1/algo/consultTheOracle.js
@@ -1,50 +1,30 @@
-// Math Library Documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-​
-​
 var floor = Math.floor(1.8);
-var ceiling = Math.ceil(Math.PI);
+var ceiling = Math.ceil( Math.PI );
 var random = Math.random();
-​
+    
 console.log(floor);
 console.log(ceiling);
 console.log(random);
-​
-​
-/*
-Dice Roller
-Using what we've learned about the Math library in JavaScript, complete the following function that should return a value between 1 through 6 at random.
-​
-Bonus Challenges
-Level 1: Modify the function to roll for a dice with any side based on a sides parameter.
-Level 2: Add another parameter for the number of die to roll and return each of the rolls performed as an array.
-Level 3:Include another parameter that represents a "Dice Check" and the dice roll must meet or exceed that Dice Check value. 
-        Return "You've slain the monster!" if you meet or exceed the Dice Check, otherwise return "The monster's slain you!"
-*/
-​
-function d6() {
+
+function d6() { //roll a six sided die
     var roll = Math.random();
     // your code here
     return roll;
 }
-​
+    
 var playerRoll = d6();
 console.log("The player rolled: " + playerRoll);
-​
+
+
 /*
-Consult The Oracle (Magic 8 Ball)
 Using the following array, write a function that will answer all of our questions by randomly choosing a response
-​
-Bonus Challenges
-Level 1: Include a parameter for the array for the function to use.
-Level 2: Add another parameter for number of random responses and return an array of each result.
+make it so your function will work even if we add or remove items from this list
 */
-​
-​
 var lifesAnswers = [
     "It is certain.",
     "It is decidedly so.",
     "Without a doubt.",
-    "Yes – definitely.",
+    "Yes - definitely.",
     "You may rely on it.",
     "As I see it, yes.",
     "Most likely.",
